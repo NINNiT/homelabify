09d255f55eb82c1a39097d4e15e6664007d6cf47
xmasterninni <soelder@hotmail.com>
2021-01-17 23:18:56 +0000

    Convert gpio scripts to using pid

--- gpio/alert.py
+++ gpio/alert.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from pid import PidFile
 from influxdb import InfluxDBClient
 from datetime import datetime
 from subprocess import check_output, Popen
@@ -8,39 +9,49 @@ import pigpio
 import configparser
 import psutil
 
-#read settings file
-settings = configparser.ConfigParser()
-settings.read('settings.ini')
+with PidFile('alert', piddir='./.pid/') as p:
+    #read settings file
+    settings = configparser.ConfigParser()
+    settings.read('settings.ini')
 
-#pigpio instance
-pi = pigpio.pi()
+    #pigpio instance
+    pi = pigpio.pi()
 
-#define sampling period
-sampling_period = settings['ALERTS']['SamplingPeriod']
+    #define sampling period
+    sampling_period = settings['ALERTS']['SamplingPeriod']
 
-#define influxdb
-influx_server = settings['INFLUXDB']['Server']
-influx_port = settings['INFLUXDB']['Port']
-influx_db = settings['INFLUXDB']['Database']
-influx_client = InfluxDBClient(host=influx_server, port=influx_port, database=influx_db)
+    #define influxdb
+    influx_server = settings['INFLUXDB']['Server']
+    influx_port = settings['INFLUXDB']['Port']
+    influx_db = settings['INFLUXDB']['Database']
+    influx_client = InfluxDBClient(host=influx_server, port=influx_port, database=influx_db)
 
-#define LED
-led_pin = settings['ALERTS']['LedPin']
-led_state = False
+    #define LED
+    led_pin = settings['ALERTS']['LedPin']
+    led_state = False
 
-def check_alerts():
-    temperature = influx_client.query('select last(temp_c) from rack_temp').last
-    influx = influx_client.query
-    measurements = False
+    def check_alerts():
+        temperature_query = influx_client.query('select last(temp_c) from rack_temp').get_points()
+        for item in temperature_query:
+            temperature = item["last"]
+        influx_query = influx_client.query('select last(influxdb) from health_check').get_points()
+        for item in influx_query:
+            influx = item["last"]
+        measurements_query = influx_client.query('select last("measurements") from health_check').get_points()
+        for item in measurements_query:
+            measurements = item["last"]
 
-#loop and write data to database
-while True:
-    temperature = influx_client.query('select last(temp_c) from rack_temp').last
-    print(temperature) 
-    # if check_alerts():
-    #     pi.write(led_pin, 0)
-    # else:
-    #     pi.write(led_pin, 1)
+        if (temperature > 45 or influx == False or measurements == False):
+            print("ALERT: something is wrong!")
+            return False
+        else:
+            return True
 
-    # time.sleep(int(sampling_period))
-    time.sleep(2)
+    #loop and write data to database
+    while True:
+        if check_alerts():
+            pi.write(int(led_pin), 0)
+        else:
+            pi.write(int(led_pin), 1)
+
+        time.sleep(int(sampling_period))
--- gpio/healthcheck.py
+++ gpio/healthcheck.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from pid import PidFile
 from influxdb import InfluxDBClient
 from datetime import datetime
 from subprocess import check_output, Popen
@@ -8,56 +9,57 @@ import configparser
 import psutil
 
 
-#read settings file
-settings = configparser.ConfigParser()
-settings.read('settings.ini')
+with PidFile('healthcheck', piddir='./.pid/') as p:
+    #read settings file
+    settings = configparser.ConfigParser()
+    settings.read('settings.ini')
 
-#define sampling period
-sampling_period = settings['HEALTHCHECKS']['SamplingPeriod']
+    #define sampling period
+    sampling_period = settings['HEALTHCHECKS']['SamplingPeriod']
 
-#define influxdb
-influx_server = settings['INFLUXDB']['Server']
-influx_port = settings['INFLUXDB']['Port']
-influx_db = settings['INFLUXDB']['Database']
-influx_client = InfluxDBClient(host=influx_server, port=influx_port, database=influx_db)
+    #define influxdb
+    influx_server = settings['INFLUXDB']['Server']
+    influx_port = settings['INFLUXDB']['Port']
+    influx_db = settings['INFLUXDB']['Database']
+    influx_client = InfluxDBClient(host=influx_server, port=influx_port, database=influx_db)
 
-def check_measurements():
-    return os.path.exists(".pid/measurements.pid")
+    def check_measurements():
+        return os.path.exists(".pid/measurements.pid")
 
-def check_alert():
-    return os.path.exists(".pid/alert.pid")
+    def check_alert():
+        return os.path.exists(".pid/alert.pid")
 
-def check_influx():
-    for process in psutil.process_iter():
-        if process.name() == 'influxd':
-            return True
-    
-    return False
+    def check_influx():
+        for process in psutil.process_iter():
+            if process.name() == 'influxd':
+                return True
 
-#get data points and format for influxdb
-def get_data_points(time_stamp):
-    measurement = check_measurements()
-    alert = check_alert()
-    influxdb = check_influx()
+        return False
 
-    data_points = [
-        {
-            "measurement": "health_check",
-            "time": time_stamp,
-            "fields": {
-                "measurements.py": measurement,
-                "alert.py": alert,
-                "influxdb": influxdb
+    #get data points and format for influxdb
+    def get_data_points(time_stamp):
+        measurement = check_measurements()
+        alert = check_alert()
+        influxdb = check_influx()
+
+        data_points = [
+            {
+                "measurement": "health_check",
+                "time": time_stamp,
+                "fields": {
+                    "measurements": measurement,
+                    "alert": alert,
+                    "influxdb": influxdb
+                }
             }
-        }
-    ]
-    return data_points
+        ]
+        return data_points
 
-#loop and write data to database
-while True:
-    time_stamp = datetime.utcnow().isoformat()
-    data_points = get_data_points(time_stamp)
-    influx_client.write_points(data_points)
-    print("writing health-checks to db...")
-    print(data_points)
-    time.sleep(int(sampling_period))
+    #loop and write data to database
+    while True:
+        time_stamp = datetime.utcnow().isoformat()
+        data_points = get_data_points(time_stamp)
+        influx_client.write_points(data_points)
+        print("writing health-checks to db...")
+        print(data_points)
+        time.sleep(int(sampling_period))
