78172b22db9af2f6dcab5dd60a3f5be62226d953
xmasterninni <soelder@hotmail.com>
2021-01-20 19:26:57 +0100

    increase dht timeout

--- stop.sh
+++ stop.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+tput setaf 3; echo "Killing all services..."
 #kill api and frontend
-pkill -f index.js
+pkill -f index.js &
 
 #kill all python script
-pkill -f alert.py
-pkill -f measurements.py
-pkill -f healthcheck.py
+pkill -f alert.py &
+pkill -f measurements.py &
+pkill -f healthcheck.py &
 
+
+tput setaf 2; echo "Killed"
